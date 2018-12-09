1b1e2bf4dcdac03aef464ede6cac61c356c9df80
igorrrj <iskirskij@gmail.com>
2018-12-09 12:58:56 +0200

    Update igorrj.php

--- igorrj.php
+++ igorrj.php
@@ -1,8 +1,23 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf200
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+//https://api.telegram.org/bot740459672:AAGgcVe3L804c8Ku1yRd1iJcr6CS1BptgRk/
+<?php
+/**
+ * Telegram Bot тупо бот access token и URL.
+ */
+$access_token = 'XXXXX:XXXXXXXXXXXXXXXXXXXXXXX';
+$api = 'https://api.telegram.org/bot' . $access_token;
+/**
+ * Задаём основные переменные.
+ */
+$output = json_decode(file_get_contents('php://input'), TRUE);
+@$chat_id = $output['message']['chat']['id'];
+@$message = $output['message']['text'];
+// команды тупо-бота
+switch($message) {
+    case '/start':
+    sendMessage($chat_id, "\xF0\x9F\x93\xA1 тупобот на связи!");
+    break;
+}
 
-\f0\fs24 \cf0 https://api.telegram.org/bot740459672:AAGgcVe3L804c8Ku1yRd1iJcr6CS1BptgRk/getMe}
+function sendMessage($chat_id, $message) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message) . '&parse_mode=html');
+}
